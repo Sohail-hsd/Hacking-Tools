c389641c89100871efb9e313963d892d0d791cfb
Sohail HSD <knight.hsd19@gmail.com>
2022-01-30 16:02:29 +0500

    ARP_Spoofer

---
+++ Sniffer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/evn python
+
+import scapy.all as scapy
+from scapy.layers import http
+
+def sniff(interface):
+    scapy.sniff(iface=interface, store=False, prn=process_sniff_packet)
+
+def process_sniff_packet(packet):
+    if packet.haslayer(http.HTTPRequest):
+        print(packet)
+
+
+interface = "wlan0"
+
+sniff(interface)
+
